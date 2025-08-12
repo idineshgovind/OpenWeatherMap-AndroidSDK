b6e23e2c552be1691d6cf8f2fc8f7c099eecab1a
Dinesh <dinesh.surya@zohocorp.com>
2025-08-12 09:05:33 +0530

    initial commit

---
+++ app/src/main/java/com/dineshdev/openweathermap/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.dineshdev.openweathermap
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.dineshdev.openweathermap.ui.theme.OpenWeatherMapAndroidSDKTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            OpenWeatherMapAndroidSDKTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    OpenWeatherMapAndroidSDKTheme {
+        Greeting("Android")
+    }
+}
